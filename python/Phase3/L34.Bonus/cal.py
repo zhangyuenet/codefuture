32121cc76b9f4b5119e96a5e249eae1d7148d6a9
zhangyue <zhangyuenet@gmail.com>
2020-09-07 13:16:47 +0800

    创建L35

---
+++ python/Phase3/L34.Bonus/cal.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import calendar
+import datetime
+from time import time,localtime,strftime
+
+# 日历
+cal = calendar.month(1900, 1)
+print(cal)
+
+#今天
+today=datetime.date.today() 
+print(today)
+# 差几天
+oneday=datetime.timedelta(days=30) 
+yesterday=today-oneday
+print(yesterday)
+
+# 获取星期等
+x=localtime(time())
+print(x)
+print(strftime('%A', x))
+print(strftime('%B', x))
+'''
+%y 两位数的年份表示（00-99）
+%Y 四位数的年份表示（000-9999）
+%m 月份（01-12）
+%d 月内中的一天（0-31）
+%H 24小时制小时数（0-23）
+%I 12小时制小时数（01-12）
+%M 分钟数（00-59）
+%S 秒（00-59）
+%a 本地简化星期名称
+%A 本地完整星期名称
+%b 本地简化的月份名称
+%B 本地完整的月份名称
+%c 本地相应的日期表示和时间表示
+%j 年内的一天（001-366）
+%p 本地A.M.或P.M.的等价符
+%U 一年中的星期数（00-53）星期天为星期的开始
+%w 星期（0-6），星期天为星期的开始
+%W 一年中的星期数（00-53）星期一为星期的开始
+%x 本地相应的日期表示
+%X 本地相应的时间表示
+%Z 当前时区的名称
+'''
+
+# 当前日期
+d=datetime.datetime.now()
+print(d.weekday()) # d.year, d.month, d.day
+print(d.isoweekday())
+print(d.strftime("%Y-%m-%d %H:%M")) # https://www.runoob.com/python/att-time-strftime.html
+
+
+
+
+
